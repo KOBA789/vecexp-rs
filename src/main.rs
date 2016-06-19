e736d5552b22570cf91976a214fa639bd8495099
Hidekazu Kobayashi <kobahide789@gmail.com>
2016-06-19 17:22:59 +0900

    initial commit (basic implementation of VM)

---
+++ src/main.rs
@@ -0,0 +1,161 @@
+use std::io;
+use std::io::prelude::*;
+
+enum OpCode<'a> {
+    Expect(usize, &'a str, usize),
+    Fail,
+    Match(&'a str),
+    Jump(usize),
+    Next,
+}
+
+struct VM<'a> {
+    pc: usize,
+    code: Vec<OpCode<'a>>,
+}
+
+enum State<'a> {
+    Done(Option<&'a str>),
+    Going(usize)
+}
+
+impl<'a> VM<'a> {
+    fn new(code: Vec<OpCode<'a>>) -> VM<'a> {
+        VM { pc: 0, code: code }
+    }
+
+    fn exec<'b>(&'b mut self, scanner: &mut Scanner) -> Option<&str> {
+        while self.pc < self.code.len() {
+            let ref op = self.code[self.pc];
+            let ref next_state = match *op {
+                OpCode::Fail => State::Done(None),
+                OpCode::Match(ret) => State::Done(Some(ret)),
+                OpCode::Jump(pc) => State::Going(pc),
+                OpCode::Expect(col, pat, pc) => match scanner.expect(col, pat) {
+                    Some(ret) => State::Going(if ret { self.pc + 1 } else { pc }),
+                    None => State::Done(None),
+                },
+                OpCode::Next => match scanner.next() {
+                    true => State::Going(self.pc + 1),
+                    false => State::Done(None),
+                },
+            };
+
+            match *next_state {
+                State::Done(ret) => return ret,
+                State::Going(pc) => self.pc = pc,
+            };
+        }
+
+        None
+    }
+}
+
+trait Scanner {
+    fn expect(&self, col: usize, pat: &str) -> Option<bool>;
+    fn next(&mut self) -> bool;
+}
+
+struct OnMemoryScanner<'a> {
+    input: &'a Vec<[&'a str; 10]>,
+    position: usize,
+}
+
+impl<'a> OnMemoryScanner<'a> {
+    fn new(input: &'a Vec<[&'a str; 10]>) -> OnMemoryScanner<'a> {
+        OnMemoryScanner { input: input, position: 0 }
+    }
+
+    fn is_eos(&self) -> bool {
+        self.input.len() <= self.position
+    }
+}
+
+impl<'a> Scanner for OnMemoryScanner<'a> {
+    fn expect(&self, col: usize, pat: &str) -> Option<bool> {
+        if self.is_eos() {
+            return None;
+        }
+
+        Some(self.input[self.position][col] == pat)
+    }
+
+    fn next<'b>(&'b mut self) -> bool {
+        if self.is_eos() {
+            return false;
+        }
+
+        self.position += 1;
+
+        !self.is_eos()
+    }
+}
+
+struct StdinScanner<'a> {
+    stdin: io::StdinLock<'a>,
+    row: Vec<String>,
+}
+
+impl<'a> StdinScanner<'a> {
+    fn new(stdin: io::StdinLock<'a>) -> StdinScanner<'a>{
+        let row = vec![];
+        StdinScanner { stdin: stdin, row: row }
+    }
+}
+
+impl<'a> Scanner for StdinScanner<'a> {
+    fn expect(&self, col: usize, pat: &str) -> Option<bool> {
+        Some(self.row[col].as_str() == pat)
+    }
+
+    fn next<'b>(&'b mut self) -> bool {
+        let mut buffer = String::new();
+        match self.stdin.read_line(&mut buffer) {
+            Ok(n) => {
+                if n <= 0 {
+                    return false;
+                }
+
+                let trim = buffer.trim();
+
+                if trim == "EOS" {
+                    return false;
+                }
+
+                let split = trim.split(',');
+                self.row = split.map(|s| s.to_string()).collect::<Vec<String>>();
+
+                true
+            }
+            Err(error) => panic!(error),
+        }
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let locked = stdin.lock();
+
+    let mut scanner = StdinScanner::new(locked);
+    let mut vm = VM::new(vec![
+        OpCode::Next,
+        OpCode::Expect(0, "すもも", 100),
+        OpCode::Expect(1, "名詞", 100),
+        OpCode::Next,
+        OpCode::Expect(0, "も", 100),
+        OpCode::Expect(1, "助詞", 100),
+        OpCode::Next,
+        OpCode::Expect(0, "もも", 100),
+        OpCode::Next,
+        OpCode::Expect(0, "も", 100),
+        OpCode::Next,
+        OpCode::Expect(0, "もも", 100),
+        OpCode::Next,
+        OpCode::Expect(0, "の", 100),
+        OpCode::Next,
+        OpCode::Expect(0, "うち", 100),
+        OpCode::Match("ok"),
+        OpCode::Fail,
+    ]);
+    assert_eq!(vm.exec(&mut scanner), Some("ok"));
+}
